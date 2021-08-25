32813e21bd334e80395be5ae4c67dd49c4eb5932
Paul Whittington <pwhittington@ricelake.com>
2021-08-25 11:07:16 -0600

    Initial commit

---
+++ src/whittington/paul/Main.java
@@ -0,0 +1,13 @@
+package whittington.paul;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] agesx = {1,2,3};
+        int[] ages = new int[7];
+
+        System.out.println("hello" + agesx[0]);
+
+        double sum = 0;
+    }
+}
